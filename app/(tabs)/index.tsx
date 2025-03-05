dc165c4529a839583ccb0c1d74eeeafb738347b5
HaHT <haht@creativeforce.io>
2025-03-06 00:22:25 +0700

    init expo pre build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -63,37 +63,6 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    if (Platform.OS !== 'web') {
-      try {
-        const localUri = await captureRef(imageRef, {
-          height: 440,
-          quality: 1,
-        });
-
-        await MediaLibrary.saveToLibraryAsync(localUri);
-        if (localUri) {
-          alert('Saved!');
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      try {
-        //@ts-ignore
-        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
-          quality: 0.95,
-          width: 320,
-          height: 440,
-        });
-
-        let link = document.createElement('a');
-        link.download = 'sticker-smash.jpeg';
-        link.href = dataUrl;
-        link.click();
-      } catch (e) {
-        console.log(e);
-      }
-    }
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
